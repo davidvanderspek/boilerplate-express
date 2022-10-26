dbb5771f61ada89d42858c17dee5155fc188271b
davidvanderspek <93922303+davidvanderspek@users.noreply.github.com>
2022-10-27 00:37:15 +1100

    ab

--- myApp.js
+++ myApp.js
@@ -2,8 +2,10 @@ let express = require("express");
 let app = express();
 console.log("Hello World");
 
+const absolutePath = __dirname + "/views/index.html";
+
 app.get("/", function (req, res) {
-  res.send("Hello Express");
+  res.sendFile(absolutePath);
 });
 
 module.exports = app;
