47b123c3c5cd1a735cd92ec8021f6758520b43e8
davidvanderspek <93922303+davidvanderspek@users.noreply.github.com>
2022-10-27 00:42:30 +1100

    app

--- myApp.js
+++ myApp.js
@@ -4,6 +4,7 @@ console.log("Hello World");
 
 const absolutePath = __dirname + "/views/index.html";
 
+app.use(express.static("__dirname + /public"));
 app.get("/", function (req, res) {
   res.sendFile(absolutePath);
 });
